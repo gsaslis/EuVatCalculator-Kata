6add45def68e954b1b1895ea05259a7efafc895c
Nikolas Vourlakis <nikolas@kadmos.io>
2016-12-16 19:28:30 +0200

    Add Javascript version

---
+++ Javascript/test/VatCalculator_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('VatCalculator', function() {
+  it('will pass', function() {
+    expect(false).to.be.true;
+  });
+});
